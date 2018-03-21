2ff1336ef845556ba2dfbce6c842ff2c62af6af6
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-03-21 12:03:30 -0700

    Animation for trash can icon implemented

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -25,7 +25,7 @@ $("input[type='text']").keypress(function(event){
 		var todoText = $(this).val();
 		$(this).val("");
 		//create a new li and add to ul
-		$("ul").append("<li><span> X </span>"+todoText+"</li>");
+		$("ul").append("<li><span><i class='far fa-trash-alt'></i></span> "+todoText+"</li>");
 	}
 	;
 })
